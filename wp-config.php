98e365fad4dbf8053d47ffab22b0581c6c369cf4
imajano <ignacio@majano.es>
2020-04-29 22:08:09 +0200

    mas

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'userOK0' );
 define( 'DB_PASSWORD', '3HUBFTlHVsQNdF8W' );
 
 /** MySQL hostname */
-define( 'DB_HOST', '172.30.136.84/mysql' );
+define( 'DB_HOST', '172.30.136.84/aytovillamuelas' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
