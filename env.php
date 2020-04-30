168576897828bdd4bde5e030542bc35caca1d89e
imajano <ignacio@majano.es>
2020-04-30 14:51:28 +0200

    Come on

--- env.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo getenv("MYSQL_USER").'<br />';
-echo getenv("MYSQL_PASSWORD").'<br />';
-echo getenv("MYSQL_ROOT_PASSWORD").'<br />';
-echo getenv("MYSQL_DATABASE").'<br />';
-echo getenv("MYSQL_SERVICE_HOST");
-?>
