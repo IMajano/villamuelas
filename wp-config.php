357eb314aa15a7e6657cef16721b58b810e653d1
imajano <ignacio@majano.es>
2020-04-30 14:32:13 +0200

    Reconfiguracion

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv("MYSQL_DATABASE") );
+define( 'DB_NAME', getenv("DB_NAME"));
 
 /** MySQL database username */
-define( 'DB_USER', getenv("MYSQL_USER") );
+define( 'DB_USER', getenv("DB_USER"));
 
 /** MySQL database password */
-define( 'DB_PASSWORD', getenv("MYSQL_PASSWORD") );
+define( 'DB_PASSWORD', getenv("DB_PASS"));
 
 /** MySQL hostname */
-define( 'DB_HOST', getenv("MYSQL_SERVICE_HOST") );
+define( 'DB_HOST', getenv("MYSQL_SERVICE_HOST"));
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
